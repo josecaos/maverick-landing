72c6b7338d9124f531a0ceb92e10b3b776871178
josecaos <jazzvoon@gmail.com>
2017-12-10 11:32:09 -0600

    Submodulo ed estilos + estructura base php + nuevos archivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once "header.php";
+
+
+include_once "secciones/splash.php";
+include_once "secciones/descripcion.php";
+include_once "secciones/thumbnails.php";
+include_once "secciones/contacto.php";
+
+
+include_once "footer.php";
