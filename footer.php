39a1432d29a80f935dc9a2a2f2b0be4e54b9d5ad
josecaos <jazzvoon@gmail.com>
2017-12-19 03:00:14 -0600

    Ajustes cliente, listos

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <footer class="footer grid-x p-t-2 p-b-2 color-secundario-5-bg color-secundario-1">
 
   <div class="footer-item small-12 medium-4 h-a text-center">
-    <i class="fa fa-globe icono-footer"> </i>&nbsp; <a href="#" target="_blank">Visita nuestio sitio</a>
+    <i class="icono-footer fa fa-globe"> </i>&nbsp; <a href="#" target="_blank">Visita nuestio sitio</a>
   </div>
   <div class="footer-item small-12 medium-4 h-a text-center">
     Maverick&nbsp;&nbsp;<i class="fa fa-copyright"></i>&nbsp;&nbsp;2017. All right reserved
--- form/mail.php
+++ form/mail.php
@@ -2,20 +2,7 @@
 
 
 if($_SERVER["REQUEST_METHOD"] === "POST") {
-	// $secret = "6LdH0x0TAAAAAGj7PZNGgsdrmzJQXg2lH6ac8HAN";
-	// $rip = $_SERVER['REMOTE_ADDR'];
-	// $captcha = $_POST['captchaResponse']; ##$_POST['g-recaptcha-response'] este era el pedo pff
-	// $respuesta  = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha");//&remoteip=$rip
-	// var_dump($respuesta);
-	// $respuesta = json_decode($respuesta,true);
-	//booleano para captcha
-	// if( $respuesta['success'] === true) {
-		// echo "El correo se ha enviado exitosamente, gracias."; //debug
-		// echo $respuesta; //debug
-		// empieza booleando para enviar mail
-		// echo "<script>$('#mensaje_res').html('El correo se ha enviado exitosamente, gracias.'); </script>";
-		if(isset($_POST['contactFormSubmitted'])) {
-			// Formulario data
+	if(isset($_POST['contactFormSubmitted'])) {
 			$name = $_POST['name'];
 			$interest = $_POST['interest'];
 			$email = $_POST['email'];
@@ -35,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 			$subject = "$name para $interest";
 			$mailheader = "De: $email \r\n";
 			mail($recipient, $subject, $message, $mailheader) or die("Algo ha salido mal, intente nuevamente.");
-			echo "<script>$('#formResponse').html('Gracias! tu correo se ha enviado exitosamente, te contactaremos a la brevedad.'); </script>";
+			echo "<script>$('#formResponse').html('Gracias! tu correo se ha enviado exitosamente.'); </script>";
 		}
 
 
