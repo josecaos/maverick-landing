c76a1752794a843ee1ee499147ffa78b85ae22aa
josecaos <jazzvoon@gmail.com>
2018-01-16 18:23:53 -0600

    Mini fix a numero de caracteres en nombre y telefono

--- form/mail.php
+++ form/mail.php
@@ -10,16 +10,16 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 			$email = $_POST['email'];
 			$phone = $_POST['telephone'];
 
-			// if (strlen($name) < 3) {
-			// 	exit("Necesitamos más caracteres en tu nombre.");
-			// }
-			//  else if (strlen($phone) < 10) {
-			// 		exit("Necesitamos más números en tu teléfono.");
-			// }
+			if (strlen($name) < 3) {
+				exit("Necesitamos más caracteres en tu nombre.");
+			}
+			 else if (strlen($phone) < 10) {
+					exit("Necesitamos 10 números en tu teléfono.");
+			}
 
 			$formcontent="Te estan contactando desde tu sitio \n\nNombre: $name\n\nApellido: $secondname\n\nInterés: $interest\n\nTeléfono: $phone\n\nCorreo: $email\n\n";
 			$message = wordwrap($formcontent, 70, "\r\n");
-			$recipient = "jazzvoon@gmail.com, danielafuentesweb@gmail.com";//"mario@maverick.com.mx";
+			$recipient = "";//correo aqui
 			$subject = $name . " " . $secondname . " para $interest";
 			$mailheader = "De: $email \r\n";
 			mail($recipient, $subject, $message, $mailheader) or die("Algo ha salido mal, intente nuevamente.");
